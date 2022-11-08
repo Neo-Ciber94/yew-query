819394f65622dadb290c7734175b1589e3237f1f
Freddy L <neo-ciber_94@hotmail.com>
2022-11-07 21:31:44 -0400

    feat: Updated common hooks

--- packages/yew-query/src/hooks/common/use_on_online.rs
+++ packages/yew-query/src/hooks/common/use_on_online.rs
@@ -1,3 +1,5 @@
+use std::rc::Rc;
+
 use wasm_bindgen::{prelude::Closure, JsCast};
 use web_sys::{window, Event};
 use yew::use_effect_with_deps;
@@ -10,15 +12,22 @@ where
         move |_| {
             let window = window().unwrap();
 
-            let on_online =
-                { Closure::wrap(Box::new(move |_: Event| callback()) as Box<dyn FnMut(_)>) };
+            let on_online = Rc::new(Closure::wrap(
+                Box::new(move |_: Event| callback()) as Box<dyn FnMut(_)>
+            ));
 
             window
-                .add_event_listener_with_callback("online", &on_online.as_ref().unchecked_ref())
+                .add_event_listener_with_callback("online", &(&*on_online).as_ref().unchecked_ref())
                 .unwrap();
 
-            on_online.forget();
-            || {}
+            move || {
+                window
+                    .remove_event_listener_with_callback(
+                        "online",
+                        &(&*on_online).as_ref().unchecked_ref(),
+                    )
+                    .unwrap();
+            }
         },
         (),
     );
--- packages/yew-query/src/hooks/common/use_on_window_focus.rs
+++ packages/yew-query/src/hooks/common/use_on_window_focus.rs
@@ -1,3 +1,5 @@
+use std::rc::Rc;
+
 use wasm_bindgen::{prelude::Closure, JsCast};
 use web_sys::{window, Event};
 use yew::use_effect_with_deps;
@@ -13,22 +15,31 @@ where
     use_effect_with_deps(
         move |first_render| {
             let window = window().unwrap();
-            let cleanup = || ();
+            let closure = Rc::new(Closure::wrap(Box::new(move |_: Event| {
+                callback();
+            }) as Box<dyn FnMut(_)>));
+
+            let cleanup = {
+                let window = window.clone();
+                let closure = closure.clone();
+                move || {
+                    window
+                        .remove_event_listener_with_callback(
+                            "focus",
+                            &(&*closure).as_ref().unchecked_ref(),
+                        )
+                        .unwrap();
+                }
+            };
 
             if *first_render {
                 return cleanup;
             }
 
-            let cb = Closure::wrap(Box::new(move |_: Event| {
-                callback();
-                log::trace!("focus");
-            }) as Box<dyn FnMut(_)>);
-
             window
-                .add_event_listener_with_callback("focus", &cb.as_ref().unchecked_ref())
+                .add_event_listener_with_callback("focus", &(&*closure).as_ref().unchecked_ref())
                 .unwrap();
 
-            cb.forget();
             cleanup
         },
         first_render,
