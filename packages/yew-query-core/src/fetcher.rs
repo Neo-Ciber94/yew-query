88fc37b3856c33782bfe2957cf78c933dd6939dd
Freddy L <neo-ciber_94@hotmail.com>
2023-01-02 22:32:31 -0400

    docs

--- packages/yew-query-core/src/fetcher.rs
+++ packages/yew-query-core/src/fetcher.rs
@@ -3,12 +3,22 @@ use futures::{Future, TryFutureExt};
 use std::{pin::Pin, rc::Rc};
 
 /// Represents a future that resolves to a `Result<T, E>`.
-pub type TryBoxFuture<T, E = Error> = Pin<Box<dyn Future<Output = Result<T, E>>>>;
+type TryBoxFuture<T, E = Error> = Pin<Box<dyn Future<Output = Result<T, E>>>>;
+
+/// Represents a function to get data.
+pub trait Fetch<T> {
+    /// The future returning the data.
+    type Fut: Future<Output = Result<T, Error>>;
+
+    /// Returns a future that resolves to the data.
+    fn get(&self) -> Self::Fut;
+}
 
 /// Boxes a `Fetcher`.
 pub struct BoxFetcher<T>(Rc<dyn Fn() -> TryBoxFuture<T>>);
 
 impl<T> BoxFetcher<T> {
+    /// Constructs a new `BoxFetcher`.
     pub fn new<F, Fut, E>(fetcher: F) -> Self
     where
         F: Fn() -> Fut + 'static,
@@ -29,6 +39,15 @@ impl<T> BoxFetcher<T> {
     }
 }
 
+impl<T> Fetch<T> for BoxFetcher<T> {
+    type Fut = TryBoxFuture<T, Error>;
+
+    fn get(&self) -> Self::Fut {
+        let ret = (self.0)();
+        ret
+    }
+}
+
 impl<T> Clone for BoxFetcher<T> {
     fn clone(&self) -> Self {
         Self(self.0.clone())
@@ -37,12 +56,30 @@ impl<T> Clone for BoxFetcher<T> {
 
 impl<T> std::fmt::Debug for BoxFetcher<T> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-       write!(f, "BoxFetcher")
+        write!(f, "BoxFetcher")
     }
 }
 
-pub struct InfiniteFetcher<T>(Box<dyn Fn(usize) -> TryBoxFuture<T>>);
+impl<F, Fut, T, E> Fetch<T> for F
+where
+    F: Fn() -> Fut + 'static,
+    Fut: Future<Output = Result<T, E>> + 'static,
+    T: 'static,
+    E: Into<Error> + 'static,
+{
+    type Fut = TryBoxFuture<T, Error>;
 
+    fn get(&self) -> Self::Fut {
+        let fut = (self)();
+        let ret = fut.map_err(|e| e.into());
+        Box::pin(ret)
+    }
+}
+
+#[allow(dead_code)]
+struct InfiniteFetcher<T>(Box<dyn Fn(usize) -> TryBoxFuture<T>>);
+
+#[allow(dead_code)]
 impl<T> InfiniteFetcher<T> {
     pub fn new<F, Fut, E>(fetcher: F) -> Self
     where
@@ -65,39 +102,5 @@ impl<T> InfiniteFetcher<T> {
 
     pub fn get(&self, param: usize) -> TryBoxFuture<T> {
         (self.0)(param)
-    }
-}
-
-/// Represents a function to get data.
-pub trait Fetch<T> {
-    /// The future returning the data.
-    type Fut: Future<Output = Result<T, Error>>;
-
-    /// Returns a future that resolves to the data.
-    fn get(&self) -> Self::Fut;
-}
-
-impl<Func, F, T, E> Fetch<T> for Func
-where
-    Func: Fn() -> F + 'static,
-    F: Future<Output = Result<T, E>> + 'static,
-    T: 'static,
-    E: Into<Error> + 'static,
-{
-    type Fut = TryBoxFuture<T, Error>;
-
-    fn get(&self) -> Self::Fut {
-        let fut = (self)();
-        let ret = fut.map_err(|e| e.into());
-        Box::pin(ret)
-    }
-}
-
-impl<T> Fetch<T> for BoxFetcher<T> {
-    type Fut = TryBoxFuture<T, Error>;
-
-    fn get(&self) -> Self::Fut {
-        let ret = (self.0)();
-        ret
     }
 }
