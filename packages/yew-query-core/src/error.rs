1ae8dd55391a57a8c28ca380ea34cf3ab20892b2
Freddy L <neo-ciber_94@hotmail.com>
2023-01-05 23:28:19 -0400

    feat: Added StaleValue error

--- packages/yew-query-core/src/error.rs
+++ packages/yew-query-core/src/error.rs
@@ -1,4 +1,4 @@
-use crate::key::QueryKey;
+use crate::QueryKey;
 use std::fmt::Display;
 use std::sync::Arc;
 
@@ -51,10 +51,17 @@ pub struct KeyNotFoundError(String);
 /// An error ocurred in a query.
 #[derive(Debug)]
 pub enum QueryError {
+    /// If the value of the query cannot be converted to the given type.
     TypeMismatch(TypeMismatchError),
+
+    /// If there is not query associated with a key.
     KeyNotFound(KeyNotFoundError),
+
+    /// If the query exists but still fetching.
     NotReady,
-    NoCacheValue,
+
+    /// If the query exists but is stale.
+    StaleValue,
 }
 
 impl QueryError {
@@ -64,8 +71,7 @@ impl QueryError {
     }
 
     pub(crate) fn key_not_found(key: &QueryKey) -> Self {
-        let k = key.to_string();
-        QueryError::KeyNotFound(KeyNotFoundError(k))
+        QueryError::KeyNotFound(KeyNotFoundError(key.key().to_string()))
     }
 }
 
@@ -79,7 +85,7 @@ impl Display for QueryError {
             TypeMismatch(TypeMismatchError(s)) => write!(f, "invalid type `{s}`"),
             KeyNotFound(KeyNotFoundError(k)) => write!(f, "key not found `{k}`"),
             NotReady => write!(f, "query had not resolved yet"),
-            NoCacheValue => write!(f, "no value in cache"),
+            StaleValue => write!(f, "value is tale"),
         }
     }
 }
