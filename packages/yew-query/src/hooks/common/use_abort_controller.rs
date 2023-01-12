4aa2c389033204a82b06b1ce6be0e8c03e85ecf2
Freddy L <neo-ciber_94@hotmail.com>
2023-01-12 01:11:24 -0400

    fix: changed use_effect to use_effect_with_deps to only trigger once

--- packages/yew-query/src/hooks/common/use_abort_controller.rs
+++ packages/yew-query/src/hooks/common/use_abort_controller.rs
@@ -1,26 +1,24 @@
 use web_sys::AbortController;
-use yew::{use_effect, use_mut_ref, hook};
+use yew::{hook, use_effect_with_deps, use_mut_ref};
 
 use super::use_is_first_render;
 
 #[hook]
 pub fn use_abort_controller() -> AbortController {
     let controller_ref = use_mut_ref(get_abort_controller);
-    let controller = {
-        controller_ref.borrow().clone()
-    };
+    let controller = { controller_ref.borrow().clone() };
     let is_first_render = use_is_first_render();
 
-    use_effect(move || {
-        let cleanup = || ();
+    use_effect_with_deps(
+        move |_| {
+            if is_first_render {
+                return;
+            }
 
-        if is_first_render {
-            return cleanup;
-        }
-
-        *controller_ref.borrow_mut() = get_abort_controller();
-        cleanup
-    });
+            *controller_ref.borrow_mut() = get_abort_controller();
+        },
+        (),
+    );
 
     controller
 }
--- packages/yew-query/src/hooks/common/use_is_first_render.rs
+++ packages/yew-query/src/hooks/common/use_is_first_render.rs
@@ -1,14 +1,16 @@
-use yew::{use_effect, use_mut_ref, hook};
+use yew::{hook, use_effect_with_deps, use_mut_ref};
 
 #[hook]
 pub fn use_is_first_render() -> bool {
     let first_render_ref = use_mut_ref(|| true);
     let first_render = *first_render_ref.borrow();
 
-    use_effect(move || {
-        *first_render_ref.borrow_mut() = false;
-        || ()
-    });
+    use_effect_with_deps(
+        move |_| {
+            *first_render_ref.borrow_mut() = false;
+        },
+        (),
+    );
 
     first_render
 }
